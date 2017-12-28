10950747febba5475adf58510eaa3bf53b06a523
nutanwaykar <waykar.nutan@gmail.com>
2017-12-28 15:05:47 +0530

    commited on master

--- src/main/java/control/DataServlet.java
+++ src/main/java/control/DataServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 @WebServlet(urlPatterns = "/data")
 public class DataServlet extends HttpServlet {
 
