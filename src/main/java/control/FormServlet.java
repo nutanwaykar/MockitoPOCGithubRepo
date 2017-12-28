a63220e04cbb09f64cbddeec323b5dac5572873b
nutanwaykar <waykar.nutan@gmail.com>
2017-12-28 15:41:12 +0530

    commited on 3.41

--- src/main/java/control/FormServlet.java
+++ src/main/java/control/FormServlet.java
@@ -17,7 +17,6 @@ public class FormServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
 		response.setContentType("text/html");
 		
 		PrintWriter out = response.getWriter();
